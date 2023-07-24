0e82a8cbb2de6b6ed8ef19cc89743154f89803c6
Harkirats043 <Harkirats043@gmail.com>
2023-07-24 10:56:43 -0400

    update question

---
+++ main.js
@@ -0,0 +1,49 @@
+
+const myQuestions = [ 
+ {
+    question1: "What is the capital of Canada?",
+    answers: [
+    {option: "Toronto", correct: false},
+    {option: "Montreal", correct: false},
+    {option: "Ottawa", correct: true},
+    {option: "Vancouver", correct: false}
+]
+},
+ {
+    question2: "How many eyes does a Bee have?",
+    answers: [
+    {option: "5", correct: true},
+    {option: "2", correct: false},
+    {option: "4", correct: false},
+    {option: "3", correct: false}
+]
+}, 
+ {
+    question3: "How many ribs are in human body?",
+    answers: [
+    {option: "20", correct: false},
+    {option: "24", correct: true},
+    {option: "22", correct: false},
+    {option: "18", correct: false}
+]
+}, 
+ {
+    question4: "What animal have same teeth as humans?",
+    answers: [
+    {option: "Giraffe", correct: true},
+    {option: "Lion", correct: false},
+    {option: "Bear", correct: false},
+    {option: "Horse", correct: false}
+]
+},  
+ {
+    question5: "Which is the only edible food that never goes bad?",
+    answers: [
+    {option: "Rice", correct: false},
+    {option: "Seeds", correct: false},
+    {option: "Dried Beans", correct: false},
+    {option: "Honey", correct: true}
+]
+}
+]
+
